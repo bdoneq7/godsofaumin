8883c030e0a339937c18d3bf43ac0bdbeff4de2d
Brian Done <briguy2078@gmail.com>
2023-10-10 18:21:30 -0400

    updated buy and resources component

--- src/components/Resources/index.js
+++ src/components/Resources/index.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import cover from "../../assets/images/cover.jpg";
 
-function Buy(props) {
+function Resources(props) {
   return (
     <section>
-      <h1 className="mx-2" id="buy">Buy Gods of Aumin</h1>
+      <h1 className="mx-2" id="resources">Gods of Aumin Resources</h1>
       <p>
-        Buy the Gods of Aumin Hardcover Book on Amazon
+        Get resources to support your Gods of Aumin Game.
         </p>
       <img src={cover} className="my-2" style={{ width: "50%", height: "50%" }} alt="Gods of Aumin" title="Gods of Aumin" />
       <div className="my-2 mx-2">
@@ -16,4 +16,4 @@ function Buy(props) {
   );
 }
 
-export default Buy;
+export default Resources;
