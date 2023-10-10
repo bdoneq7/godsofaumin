4cbffe6f7b6217d5371c0d224eaad44db4cc28c4
Brian Done <briguy2078@gmail.com>
2023-10-10 17:59:08 -0400

    updated nav css

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -5,11 +5,12 @@ function About(props) {
   return (
     <section>
       <h1 className="mx-2" id="about">Gods of Aumin</h1>
+      <p>
+        Gods of Aumin Table Top RPG where characters can progress to become Gods and play as Gods.
+        </p>
       <img src={cover} className="my-2" style={{ width: "50%", height: "50%" }} alt="Gods of Aumin" title="Gods of Aumin" />
       <div className="my-2 mx-2">
-        <p>
-        Gods of Aumin Table Top RPG
-        </p>
+        
       </div>
     </section>
   );
